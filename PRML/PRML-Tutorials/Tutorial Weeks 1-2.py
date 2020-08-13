5364d75eb66467b63f3f598eb517a23a43392436
BigBingoBoi <alekdras@gmail.com>
2020-08-13 12:28:51 +1000

    first commit

---
+++ PRML/PRML-Tutorials/Tutorial Weeks 1-2.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 10:30:48 2020
+
+@author: Aleks
+"""
+# =============================================================================
+# 
+# # Exercise 1: The First Running
+# print("Hello, World!")
+# 
+# 
+# # Exercise 2: Values and Operators
+# print(5 + 4) 
+# print(5.0 + 4.0)
+# print((5 + 4) * 8)
+# print((5.0 + 4) * 8)
+# print(-5 * -4 + 2)
+# print(4 ** 2 + 6 )
+# print(-4 ** 2 + 6)
+# print((9 + 1) * 5 / 2)
+# print((10 - 6) / ((3 * 2) - (15 / 5)))
+# 
+# 
+# # Exercise 3: Types of Values
+# print(type(5 + 4))
+# print(type(5.0 + 4.0))
+# print(type((5 + 4) * 3))
+# print(type((4.0 + 5) * 3))
+# print(type((9 + 1) * 5 / 2))
+# print(type("Hello World"))
+# 
+# 
+# # Exercise 4: Operators on Different Types
+# print("Hello" + "World")
+# print(type("Hello" + "World"))
+# print("Abc" * 3)
+# print("Abc" * 3.0)
+# print("123" * 3)
+# print("36" / 12)
+# print("36" / "3")
+# print([1, 2, 3] + [4, 5, 6] * 3)
+# print(["Hello", "World"] * 3)
+# print(type(["Hello", "World"] * 3))
+# print([1, 2, 3] * [3])
+# 
+# =============================================================================
+
+# Exercise 5
+x = int(input("Enter a number: "))
+y = x ** 2
+print(f"The square of {x} is {y}")
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a second number:" ))
+c = a * b
+print(f"{a} multiplied by {b} equals {c}")
